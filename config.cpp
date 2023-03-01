6a9011996a760b6926b4f8644122dc16528787b9
Bromine <116853874+Brominum@users.noreply.github.com>
2023-03-01 01:53:37 -0500

    Initial Commit

---
+++ config.cpp
@@ -0,0 +1,365 @@
+class CfgPatches
+{
+	class Bro_H60_Camos
+	{
+		name="[Bro] H60 - Camos";
+		units[]= {};
+		weapons[]={};
+		requiredVersion=1.5599999;
+		requiredAddons[]=
+		{
+			"vtx_main"
+		};
+		author="";
+		authors[]=
+		{
+			""
+		};
+		version="0.6.0.26";
+		versionStr="0.6.0.26";
+		versionAr[]={0,6,0,26};
+		magazines[]={};
+	};
+};
+//Comment
+class CfgVehicles
+{
+	class Helicopter_Base_H{};
+	class Heli_Transport_01_base_F: Helicopter_Base_H{};
+	class vtx_H60_base: Heli_Transport_01_base_F
+	{
+		class textureSources
+		{
+			class Bro_Tan
+			{
+				displayName = "[Bro] Tan";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\Tan\Hull_Main_co.paa",
+					"Bro_H60_Skins\Tan\Misc_co.paa",
+					"Bro_H60_Skins\Tan\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};
+			class Bro_Camo1
+			{
+				displayName = "[Bro] Green Camo";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\Camo1\Hull_Main_co.paa",
+					"Bro_H60_Skins\Camo1\Misc_co.paa",
+					"Bro_H60_Skins\Camo1\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};
+			class Bro_Camo2
+			{
+				displayName = "[Bro] Blue Camo";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\Camo2\Hull_Main_co.paa",
+					"Bro_H60_Skins\Camo2\Misc_co.paa",
+					"Bro_H60_Skins\Camo2\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};
+			class Bro_MC
+			{
+				displayName = "[Bro] Multicam";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\MC\Hull_Main_co.paa",
+					"Bro_H60_Skins\MC\Misc_co.paa",
+					"Bro_H60_Skins\MC\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};
+			class Bro_RedYellow
+			{
+				displayName = "[Bro] Red/Yellow Stripes";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\RedYellow\Hull_Main_co.paa",
+					"Bro_H60_Skins\RedYellow\Misc_co.paa",
+					"Bro_H60_Skins\RedYellow\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};	
+			class Bro_BlueWhite
+			{
+				displayName = "[Bro] Blue/White Stripes";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\BlueWhite\Hull_Main_co.paa",
+					"Bro_H60_Skins\BlueWhite\Misc_co.paa",
+					"Bro_H60_Skins\BlueWhite\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};
+			class Bro_Digi1
+			{
+				displayName = "[Bro] Green Digital";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\Digi1\Hull_Main_co.paa",
+					"Bro_H60_Skins\Digi1\Misc_co.paa",
+					"Bro_H60_Skins\Digi1\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};
+			class Bro_Digi2
+			{
+				displayName = "[Bro] Blue Digital";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\Digi2\Hull_Main_co.paa",
+					"Bro_H60_Skins\Digi2\Misc_co.paa",
+					"Bro_H60_Skins\Digi2\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};
+			class Bro_LV
+			{
+				displayName = "[Bro] LV Pattern";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\LV\Hull_Main_co.paa",
+					"Bro_H60_Skins\LV\Misc_co.paa",
+					"Bro_H60_Skins\LV\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};
+			class Bro_MT
+			{
+				displayName = "[Bro] Missing Texture";
+				author = "Bromine";
+				textures[] = {
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"",
+					"Bro_H60_Skins\MT\Hull_Main_co.paa",
+					"Bro_H60_Skins\MT\Misc_co.paa",
+					"Bro_H60_Skins\MT\Hull_Tail_co.paa",
+					"",
+					"",
+					"",
+					"",
+					"",
+					""
+				};
+				factions[] = {};
+			};				
+		};
+	};
+};
